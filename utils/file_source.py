1de92f9b952f469833991c62c748d84918bb57cf
liujiniang <liujiniang12345@126.com>
2021-12-05 15:25:22 +0800

    修改

--- utils/file_source.py
+++ utils/file_source.py
@@ -19,8 +19,10 @@ class FileSource(Source):
                     continue
 
                 item = line.split(',', 1)
-
-                threads.add_task(self.detectData, title = item[0], url = item[1])
+                
+                urls = item[1].split('#')
+                for j in range(0, len(urls)):
+                    threads.add_task(self.detectData, title = item[0], url = urls[j])
                 # thread = threading.Thread(target = self.detectData, args = (item[0], item[1], ), daemon = True)
                 # thread.start()
                 # threads.append(thread)
--- utils/source.py
+++ utils/source.py
@@ -34,7 +34,29 @@ class Source:
             self.addData(data)
             self.T.logger('正在分析[ %s ]: %s' % (str(info['id']) + str(info['title']), url))
         else :
-            pass # MAYBE later :P
+            data = {
+                'title'  : str(info['id']) if info['id'] != '' else str(info['title']),
+                'url'    : str(url),
+                'quality': str(info['quality']),
+                'delay'  : netstat,
+                'level'  : str(info['level']),
+                'cros'   : cros,
+                'online' : 1,
+                'udTime' : self.now,
+                'logo': epg.logo,
+                'group_name': epg.group,
+                'tvg_id': epg.tvgId,
+                'tvg_name': epg.tvgName,
+            }
+
+            DB = DataBase()
+            sql = "SELECT * FROM %s WHERE url = '%s'" % (DB.table, data['url'])
+            result = DB.query(sql)
+            if len(result) > 0 :
+                id = result[0][0]
+                DB.edit(id, data)
+                print("update=" + sql)           
+
     def addData (self, data) :
         DB = DataBase()
         sql = "SELECT * FROM %s WHERE url = '%s'" % (DB.table, data['url'])

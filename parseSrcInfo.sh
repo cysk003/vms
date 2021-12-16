d9bc597a4f7bfb53a0ea052ceb2103a6c1b28245
liujinliang <liujinliang@bjca.org.cn>
2021-12-16 14:01:30 +0800

    下载

--- parseSrcInfo.sh
+++ parseSrcInfo.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-outPageDetail(){
-    baseUrl=$1
-    listFile=$2
-    extractFile=$3
-    prefix=$baseUrl"/"
-    postfix=".html"
-    baseInfos=$(curl $baseUrl/$extractFile 2>/dev/null | grep "<a "|grep title|grep class=|grep "bookmark"|awk '{print $2"^|^"$5}')
-    for baseInfo in $baseInfos
-    do
-      title=$(echo $baseInfo|awk -F'"' '{print $4}')
-      [ -z "$title" ] && echo "title为空==>"$baseInfo
-      url=$(echo $baseInfo |awk -F'"' '{print $2}')
-      len=$(( ${#url}-${#postfix}-${#prefix} + 1 ))
-      id=${url:$((${#prefix} - 1 )):$len}
-       echo "开始-->$title:$url,$id"
+parseDetail(){
+      url=$1
+      title=$2
+      listFile=$3
+      group=$4
+
       page=$(curl $url 2>/dev/null|grep "\<a "|grep "DownSys"|sed 's/ /^|^/g')
       for detail in $page;
       do
@@ -27,7 +18,7 @@ outPageDetail(){
             realBaseUrl=${videoUrl%/share/*}
             m3u8File=$(curl $videoUrl 2>/dev/null|grep playlist |grep url|awk -F '"' '{print $4}')
             if [ ! -z "$m3u8File" ]; then
-               echo $title $playType,$realBaseUrl$m3u8File >> $listFile
+               echo $group,$title $playType,$realBaseUrl$m3u8File"?skip=1"|sed 's/ //g' >> $listFile
             else
                echo "$videoUrl 没找到m3u8"
             fi
@@ -39,13 +30,33 @@ outPageDetail(){
           if [ ! -z "$m3u8File" ]; then
             curl $m3u8File 2>/dev/null 1>/dev/null
             if [ "$?" == "0" ]; then
-              echo $title $playType,$m3u8File >> $listFile
+              echo $group,$title $playType,$m3u8File"?skip=1" >> $listFile
             else
               echo "$m3u8File 访问不通"
             fi
          fi
         fi
       done
+
+}
+
+outPageDetail(){
+    baseUrl=$1
+    listFile=$2
+    extractFile=$3
+    group=$4
+    prefix=$baseUrl"/"
+    postfix=".html"
+    baseInfos=$(curl $baseUrl/$extractFile 2>/dev/null | grep "<a "|grep title|grep class=|grep "bookmark"|awk '{print $2"^|^"$5}')
+    for baseInfo in $baseInfos
+    do
+      title=$(echo $baseInfo|awk -F'"' '{print $4}')
+      [ -z "$title" ] && echo "title为空==>"$baseInfo
+      url=$(echo $baseInfo |awk -F'"' '{print $2}')
+      len=$(( ${#url}-${#postfix}-${#prefix} + 1 ))
+      id=${url:$((${#prefix} - 1 )):$len}
+      echo "开始-->$title:$url"
+      parseDetail $url $title $listFile $group
     done
 }
 
@@ -53,16 +64,17 @@ getPageList() {
   baseUrl=$1
   listFile=$2
   end=$3
-   echo "抓取$baseUrl/index.html"
-  outPageDetail $baseUrl $listFile "index.html"
+  group=$4
+  echo "抓取$baseUrl/index.html"
+  outPageDetail $baseUrl $listFile "index.html" $group
   for i in `seq 2 $end`;
   do
       echo "抓取$baseUrl/index_$i.html"
-      outPageDetail $baseUrl $listFile "index_$i.html"
+      outPageDetail $baseUrl $listFile "index_$i.html"  $group
   done
 }
 
 #getPageList https://www.ai66.cc/kehuanpian/ ai66/kehuan.txt 38
 #getPageList https://www.ai66.cc/xijupian/ ai66/xijupian.txt 92
 
-getPageList https://www.ai66.cc/$1/ ai66/$1.txt $2
+getPageList https://www.ai66.cc/$1/ ai66/$1.txt $2 $3
--- utils/tools.py
+++ utils/tools.py
@@ -13,13 +13,169 @@ import time
 from .area import *
 import os
 import cv2
+import pycurl
+try:
+    from io import BytesIO
+except ImportError:
+    from io import StringIO as BytesIO
+import random
 
 socket.setdefaulttimeout(5.0)
+
+
+
+def get_header_function(header_line, headers = {}):
+    def header_function(header_line):
+        #python3上需要解码
+        header_line = header_line.decode('iso-8859-1')
+        if ':' not in header_line:
+            return
+        name, value = header_line.split(':', 1)
+        name = name.strip()
+        value = value.strip()
+        name = name.lower()
+        headers[name] = value
+    return header_function
 
 class Tools(object):
     def __init__(self):
         pass
 
+    def getPage2 (self, url, requestHeader = []) :
+        buffer = BytesIO()
+        fakeIp = self.fakeIp()
+        requestHeader.append('CLIENT-IP:' + fakeIp)
+        requestHeader.append('X-FORWARDED-FOR:' + fakeIp)
+
+        try:
+            curl = pycurl.Curl()
+            header={}
+            curl.setopt(pycurl.URL, url.strip())
+            curl.setopt(pycurl.ENCODING, 'gzip,deflate')
+            curl.setopt(pycurl.HEADER, 1)
+            curl.setopt(pycurl.TIMEOUT, 120)
+            curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+            curl.setopt(pycurl.SSL_VERIFYHOST, 0)
+            curl.setopt(pycurl.HEADERFUNCTION, get_header_function(header))
+            curl.setopt(pycurl.WRITEFUNCTION, buffer.write)
+
+            curl.perform()
+
+            headerSize = curl.getinfo(pycurl.HEADER_SIZE)
+            body = buffer.getvalue().decode('utf-8')
+
+        except Exception as e:
+            print(e)
+            print(url)
+            header = ''
+            body = ''
+
+        return header, body
+    def fakeIp (self) :
+        fakeIpList = []
+
+        for x in xrange(0, 4):
+            fakeIpList.append(str(int(random.uniform(0, 255))))
+
+        fakeIp = '.'.join(fakeIpList)
+
+        return
+    def xor (self, x, y, base = 32) :
+        stat = True
+        if x >= 0 :
+            x = str(bin(int(str(x), 10)))[2:]
+            for i in xrange(0, base - len(x)):
+                x = '0' + x
+        else :
+            x = str(bin(int(str(x + 1), 10)))[3:]
+            for i in xrange(0, base - len(x)):
+                x = '0' + x
+            t = ''
+            for i in xrange(0,len(x)):
+                if x[i] == '1' :
+                    t = t + '0'
+                else :
+                    t = t + '1'
+            x = t
+        if y >= 0 :
+            y = str(bin(int(str(y), 10)))[2:]
+            for i in xrange(0, base - len(y)):
+                y = '0' + y
+        else :
+            y = str(bin(int(str(y + 1), 10)))[3:]
+            for i in xrange(0, base - len(y)):
+                y = '0' + y
+            t = ''
+            for i in xrange(0,len(y)):
+                if y[i] == '1' :
+                    t = t + '0'
+                else :
+                    t = t + '1'
+            y = t
+        t = ''
+        for i in xrange(0, base):
+            if x[i] == y[i] :
+                t = t + '0'
+            else :
+                t = t + '1'
+        x = t
+        if x[0] == '1' :
+            stat = False
+            t = ''
+            for i in xrange(0,len(x)):
+                if x[i] == '1' :
+                    t = t + '0'
+                else :
+                    t = t + '1'
+            x = t
+        r = int(str(x), 2)
+        if stat == False :
+            r = 0 - r - 1
+
+        return r
+
+    def rotate (self, x, y, w, base = 32) :
+        stat = True
+        if x >= 0 :
+            x = str(bin(int(str(x), 10)))[2:]
+            for i in xrange(0, base - len(x)):
+                x = '0' + x
+        else :
+            x = str(bin(int(str(x + 1), 10)))[3:]
+            for i in xrange(0, base - len(x)):
+                x = '0' + x
+            t = ''
+            for i in xrange(0,len(x)):
+                if x[i] == '1' :
+                    t = t + '0'
+                else :
+                    t = t + '1'
+            x = t
+        if y >= base :
+            y = y % base
+        for i in xrange (0, y) :
+            if w != 'r+' :
+                x = x[0] + x + '0'
+            else :
+                x = '0' + x + '0'
+        if w == 'r' or w == 'r+' :
+            x = x[0 : base]
+        else :
+            x = x[(len(x) - base) : ]
+        if x[0] == '1' :
+            stat = False
+            t = ''
+            for i in xrange(0,len(x)):
+                if x[i] == '1' :
+                    t = t + '0'
+                else :
+                    t = t + '1'
+            x = t
+        r = int(str(x), 2)
+        if stat == False :
+            r = 0 - r - 1
+
+        return r
     def getPage (self, url, requestHeader = [], postData = {}) :
         fakeIp = self.fakeIp()
         requestHeader.append('CLIENT-IP:' + fakeIp)

f22304256ec72de310601bafaa676872ddb67ee3
liujiniang <liujiniang12345@126.com>
2021-12-31 20:22:48 +0800

    提交修改

--- category/taiwan/scan_iprange.sh
+++ category/taiwan/scan_iprange.sh
@@ -7,10 +7,11 @@ do
   [ "$result2" -gt 0 ] && echo  $url.$port >> category/taiwan/ip.txt ;
 done
 
-url=$1
-for port in `seq 0 254`;
+
+for id in `seq 900 2000`;
 do
-  result2=`curl -m1 --connect-time 1 $url.$port 2>/dev/null |grep player-live|wc -l`
-  echo $url.$port, $result2
-  [ "$result2" -gt 0 ] && echo  $url.$port >> category/taiwan/ip.txt ;
+  url=http://42.176.185.28:9901/tsfile/live/${id}_1.m3u8
+  result2=`curl -m1 --connect-time 1 $url 2>/dev/null |grep EXTM3U|wc -l`
+  echo $url, $result2
+  [ "$result2" -gt 0 ] && echo  $url >> category/taiwan/28.txt ;
 done

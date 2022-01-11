40b11acad65907c4228b6b098ba4746d2527ef88
liujinliang <liujinliang@bjca.org.cn>
2022-01-11 14:25:00 +0800

    增加修改

---
+++ category/aishang/scan.sh
@@ -0,0 +1,11 @@
+for i in `seq 1 9999`;
+ do
+   tvid=`printf %04d $i`
+   url="http://live.aishang.ctlcdn.com/0000011024${tvid}_1/encoder/0/playlist.m3u8?CONTENTID=0000011024${tvid}_1&AUTHINFO=FABqh274XDn8fkurD5614t%2B1RvYajgx%2Ba3PxUJe1SMO4OjrtFitM6ZQbSJEFffaD35hOAhZdTXOrK0W8QvBRom%2BXaXZYzB%2FQfYjeYzGgKhP%2Fdo%2BXpr4quVxlkA%2BubKvbU1XwJFRgrbX%2BnTs60JauQUrav8kLj%2FPH8LxkDFpzvkq75UfeY%2FVNDZygRZLw4j%2BXtwhj%2FIuXf1hJAU0X%2BheT7g%3D%3D&USERTOKEN=eHKuwve%2F35NVIR5qsO5XsuB0O2BhR0KR"
+   result=`curl -I  $url 2>/dev/null | grep "200 OK"|wc -l`
+   echo $url, $result
+   url="http://live.aishang.ctlcdn.com/0000011024${tvid}_1/playlist.m3u8?CONTENTID=0000011024${tvid}_1&AUTHINFO=FABqh274XDn8fkurD5614t%2B1RvYajgx%2Ba3PxUJe1SMO4OjrtFitM6ZQbSJEFffaD35hOAhZdTXOrK0W8QvBRom%2BXaXZYzB%2FQfYjeYzGgKhP%2Fdo%2BXpr4quVxlkA%2BubKvbU1XwJFRgrbX%2BnTs60JauQUrav8kLj%2FPH8LxkDFpzvkq75UfeY%2FVNDZygRZLw4j%2BXtwhj%2FIuXf1hJAU0X%2BheT7g%3D%3D&USERTOKEN=eHKuwve%2F35NVIR5qsO5XsuB0O2BhR0KR"
+   if [ $result -gt 0 ]; then
+       echo $i,$url >> category/aishang/id.txt
+   fi
+ done
--- newSrc/scan_daytv.sh
+++ newSrc/scan_daytv.sh
@@ -54,13 +54,15 @@ done < newSrc/harebing.txt
 
 
 http://58.244.50.82:808/hls/51/index.m3u8
-for i in `seq 1 500`;
+for i in `seq 1 9999`;
 do
-  url="http://58.244.50.82:808/hls/$i/index.m3u8"
-  result=`curl -I  $url 2>/dev/null | grep "404 Not"|wc -l`
+  tvid=`printf %04d $i`
+  url="http://live.aishang.ctlcdn.com/0000011024${tvid}_1/encoder/0/playlist.m3u8?CONTENTID=0000011024${tvid}_1&AUTHINFO=FABqh274XDn8fkurD5614t%2B1RvYajgx%2Ba3PxUJe1SMO4OjrtFitM6ZQbSJEFffaD35hOAhZdTXOrK0W8QvBRom%2BXaXZYzB%2FQfYjeYzGgKhP%2Fdo%2BXpr4quVxlkA%2BubKvbU1XwJFRgrbX%2BnTs60JauQUrav8kLj%2FPH8LxkDFpzvkq75UfeY%2FVNDZygRZLw4j%2BXtwhj%2FIuXf1hJAU0X%2BheT7g%3D%3D&USERTOKEN=eHKuwve%2F35NVIR5qsO5XsuB0O2BhR0KR"
+  result=`curl -I  $url 2>/dev/null | grep "200 OK"|wc -l`
   echo $url, $result
-  if [ $result -eq 0 ]; then
-      echo $i,$url >> newSrc/changchun.txt
+  url="http://live.aishang.ctlcdn.com/0000011024${tvid}_1/playlist.m3u8?CONTENTID=0000011024${tvid}_1&AUTHINFO=FABqh274XDn8fkurD5614t%2B1RvYajgx%2Ba3PxUJe1SMO4OjrtFitM6ZQbSJEFffaD35hOAhZdTXOrK0W8QvBRom%2BXaXZYzB%2FQfYjeYzGgKhP%2Fdo%2BXpr4quVxlkA%2BubKvbU1XwJFRgrbX%2BnTs60JauQUrav8kLj%2FPH8LxkDFpzvkq75UfeY%2FVNDZygRZLw4j%2BXtwhj%2FIuXf1hJAU0X%2BheT7g%3D%3D&USERTOKEN=eHKuwve%2F35NVIR5qsO5XsuB0O2BhR0KR"
+  if [ $result -gt 1 ]; then
+      echo $i,$url >> category/aishang/id.txt
   fi
 done
 

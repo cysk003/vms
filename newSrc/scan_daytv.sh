ac82b9b01daff594bbc17bd6c43f3ab4a46daaae
liujinliang <liujinliang@bjca.org.cn>
2022-02-22 18:54:39 +0800

    增加修改

--- newSrc/scan_daytv.sh
+++ newSrc/scan_daytv.sh
@@ -539,13 +539,14 @@ http://115.224.127.233:9901/tsfile/live/1000_1.m3u8
 110.53.159.62
 http://106.12.5.201:9901/tsfile/live/1000_1.m3u8
 http://110.72.54.126:9901/tsfile/live/0001_1.m3u8
+http://58.213.48.108:9901/tsfile/live/0001_1.m3u8
 for id in `seq 1 200` `seq 1000 1299` ;
 do
-  url="http://113.58.165.232:9901/tsfile/live/`printf %04d $id`_1.m3u8"
+  url="http://112.86.222.211:9901/tsfile/live/`printf %04d $id`_1.m3u8"
   result=`curl --connect-timeout 1 $url 2>/dev/null | grep "\.ts"|wc -l`
   echo $url, $result
   if [ $result -gt 0 ]; then
-      echo $id,$url >> category/hainan/danzhou.txt
+      echo $id,$url >> category/jiangsu/nanjing.txt
   fi
 done      
 http://119.39.192.8:9901/tsfile/live/0001_1.m3u8
